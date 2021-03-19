ed89d245c4b88b63e78c6297a7550ef77ba1e147
tom@tomspencer.co <tom@cognitives.io>
2021-03-19 17:24:58 +1100

    added bootstrap via webpack and navbar and footer

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,8 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import 'css/site'
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
